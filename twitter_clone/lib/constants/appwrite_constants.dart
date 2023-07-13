daede17dd4ff46a2108bc2c520170f6512f8146b
Simran1604 <simran251601@gmail.com>
2023-07-13 17:55:41 +0530

    realtime updated

--- twitter_clone/lib/constants/appwrite_constants.dart
+++ twitter_clone/lib/constants/appwrite_constants.dart
@@ -1,13 +1,13 @@
 class AppwriteConstants {
-  static const String databaseId = '64806a8e243455b8cb73';
-  static const String projectId = '64703cc4a8f7c1018ca5';
-  static const String endPoint = 'https://cloud.appwrite.io/v1';
+  static const String databaseId = '64af9848267e0d1a9e67';
+  static const String projectId = '64af95a8cd8a54ca128d';
+  static const String endPoint = 'http://192.168.29.253:80/v1';
 
-  static const String usersCollection = '64806ab26d4d1ae0ea46';
-  static const String tweetsCollection = '64806ad12d77f1575cba';
-  static const String notificationsCollection = '648999dd13e7cb2e4448';
+  static const String usersCollection = '64afb4e0052ec449ea52';
+  static const String tweetsCollection = '64afba04e1122f891cd7';
+  static const String notificationsCollection = '64afc7fb2037c1b69672';
 
-  static const String imagesBucket = '64809c3bdbfc067b1abb';
+  static const String imagesBucket = '64afbcfb41b46a05cd6f';
 
   static String imageUrl(String imageId) =>
       '$endPoint/storage/buckets/$imagesBucket/files/$imageId/view?project=$projectId&mode=admin';
